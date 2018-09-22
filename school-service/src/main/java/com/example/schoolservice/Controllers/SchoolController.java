f20c646af5d064cd53eeeb7bf2ce541f6a58d7a2
Suresh Chand <schand0276@gmail.com>
2018-09-22 05:46:41 -0400

    hello

---
+++ school-service/src/main/java/com/example/schoolservice/Controllers/SchoolController.java
@@ -0,0 +1,51 @@
+package com.example.schoolservice.Controllers;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+public class SchoolController {
+
+    @Autowired
+    RestTemplate restTemplate;
+
+
+
+    @RequestMapping(value="getSchoolDetails/{schoolName}",method=RequestMethod.GET)
+    public  String getStudents(@PathVariable String schoolName)
+    {
+
+        System.out.println("getting school details for "+schoolName);
+        String response = restTemplate.exchange("http://student-service/getStudentDetailsForSchool/{schoolname}", HttpMethod.GET, null, new ParameterizedTypeReference<String>() {
+        }, schoolName).getBody();
+
+
+        System.out.println("Response Received as " + response);
+
+        return "School Name -  " + schoolName + " \n Student Details " + response;
+    }
+
+
+
+
+
+
+
+
+    @Bean
+    @LoadBalanced
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+
+
+}
---
+++ sunny/src/test/java/com/sureshpoint/sunny/SunnyApplicationTests.java
@@ -0,0 +1,16 @@
+package com.sureshpoint.sunny;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SunnyApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
